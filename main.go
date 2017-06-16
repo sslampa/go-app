29d3956ce39206dc4bcbe525e666d158d9fed3ea
sslampa <sslampa@gmail.com>
2017-06-15 23:39:03 -0700

    Add log in page

--- main.go
+++ main.go
@@ -10,12 +10,13 @@ import (
 func main() {
 	port := flag.String("p", "8080", "port to serve on")
 	flag.Parse()
+	log.Printf("Serving on HTTP port: %s\n", *port)
 
 	http.HandleFunc("/", sendIndex)
 	http.HandleFunc("/user", sendUser)
+	http.HandleFunc("/login", sendLogin)
 	http.Handle("/stylesheets/", http.StripPrefix("/stylesheets/", http.FileServer(http.Dir("stylesheets"))))
 
-	log.Printf("Serving on HTTP port: %s\n", *port)
 	err := http.ListenAndServe(":"+*port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
@@ -36,6 +37,15 @@ func sendUser(w http.ResponseWriter, r *http.Request) {
 	tpl := template.Must(template.ParseGlob("./templates/*.gohtml"))
 
 	err := tpl.ExecuteTemplate(w, "user.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func sendLogin(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseGlob("./templates/*.gohtml"))
+
+	err := tpl.ExecuteTemplate(w, "login.gohtml", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
