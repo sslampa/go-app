8c3e3ccd181158c5f508c6f95679f35284c7dd7b
Stephen Lampa <sslampa@gmail.com>
2017-06-30 00:22:19 -0700

    Create user session

--- models/userSessions.go
+++ models/userSessions.go
@@ -11,10 +11,20 @@ type UserSession struct {
 // InitUserSessions creates the UserSessions table in the db
 func InitUserSessions() {
 	tableQuery := `CREATE TABLE IF NOT EXISTS user_sessions (
-    SessionID SERIAL PRIMARY KEY,
-    UserID INT REFERENCES users(id))`
+    session_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id))`
 
 	_, err := DB.Exec(tableQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// CreateUserSession makes a session
+func CreateUserSession(us *UserSession) {
+	userSessionInsert := "INSERT INTO user_sessions (session_id, user_id)"
+
+	_, err := DB.Exec(userSessionInsert, us.SessionID, us.UserID)
 	if err != nil {
 		log.Fatal(err)
 	}
